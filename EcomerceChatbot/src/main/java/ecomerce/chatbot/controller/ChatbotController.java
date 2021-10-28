65fca86729ddd92a757be54d5904e78503350902
unknown <mohamederrahali34@gmail.com>
2021-10-28 01:22:53 +0100

    add files for trainning

--- EcomerceChatbot/src/main/java/ecomerce/chatbot/controller/ChatbotController.java
+++ EcomerceChatbot/src/main/java/ecomerce/chatbot/controller/ChatbotController.java
@@ -1,5 +1,6 @@
 package ecomerce.chatbot.controller;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -10,7 +11,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.apache.tomcat.jni.File;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,13 +49,19 @@ public class ChatbotController {
 		 * Define answers for each given category.
 		 */
 		
-			questionAnswer.put("greeting", "Hello, how can I help you?");
-			questionAnswer.put("product-inquiry",
-					"Product is a TipTop mobile phone. It is a smart phone with latest features like touch screen, blutooth etc.");
-			questionAnswer.put("price-inquiry", "Price is $300");
-			questionAnswer.put("conversation-continue", "What else can I help you with?");
-			questionAnswer.put("conversation-complete", "Nice chatting with you. Bbye.");
-
+			questionAnswer.put("salutations", "Bonjour,Comment je peux vous aider ?");
+			//questionAnswer.put("product-inquiry",
+				//	"Product is a TipTop mobile phone. It is a smart phone with latest features like touch screen, blutooth etc.");
+			questionAnswer.put("demande-de-prix", "prix est $300");
+			//questionAnswer.put("conversation-continue", "What else can I help you with?");
+			//questionAnswer.put("conversation-complete", "Nice chatting with you. Bbye.");
+			
+			questionAnswer.put("demande-categorie", "Parfait ! Votre demande concerne : pantalon,T-Shirt....?");
+			questionAnswer.put("mon-choix", "Parfait , quelle taille ?");
+			questionAnswer.put("ma-taille", "Quelle est votre couleur préférée?");
+			questionAnswer.put("ma-couleur", "Pour mieux comprendre votre demande et trouver le bon produit, je vous conseil de consulter la page http://www.Shop4Yourself.com/T-shirt/Taille-M , votre facture sera entre 150Dhs ET 250Dhs selon votre chox!");
+			
+			questionAnswer.put("remerciement", "Nous vous souhaitons de bons achats ");
 		
 		// Train categorizer model to the training data we created.
 	    DoccatModel model = trainCategorizerModel();
@@ -77,10 +83,48 @@ public class ChatbotController {
 
 			// Get predefined answer from given category & add to answer.
 			answer = answer + " " + questionAnswer.get(category);
-
 		}
 		
 		
+		return answer ;
+	}
+	public String chat1(String userInput) throws FileNotFoundException, IOException {
+		Map<String, String> questionAnswer = new HashMap<>();
+
+		/*
+		 * Define answers for each given category.
+		 */
+		
+			questionAnswer.put("greeting", "Hello, how can I help you?");
+			questionAnswer.put("product-inquiry",
+					"Product is a TipTop mobile phone. It is a smart phone with latest features like touch screen, blutooth etc.");
+			questionAnswer.put("price-inquiry", "Price is $300");
+			questionAnswer.put("conversation-continue", "What else can I help you with?");
+			questionAnswer.put("conversation-complete", "Nice chatting with you. Bbye.");
+			questionAnswer.put("tacos", "tacos poulet !.");
+			
+		
+		// Train categorizer model to the training data we created.
+	    DoccatModel model = trainCategorizerModel();
+		String[] sentences = breakSentences(userInput);
+		String answer = "";
+		for (String sentence : sentences) {
+			// Separate words from each sentence using tokenizer.
+			String[] tokens = tokenizeSentence(sentence);
+
+			// Tag separated words with POS tags to understand their gramatical structure.
+			String[] posTags = detectPOSTags(tokens);
+
+			// Lemmatize each word so that its easy to categorize.
+			String[] lemmas = lemmatizeTokens(tokens, posTags);
+
+			// Determine BEST category using lemmatized tokens used a mode that we trained
+			// at start.
+			String category = detectCategory(model, lemmas);
+
+			// Get predefined answer from given category & add to answer.
+			answer = answer + " " + questionAnswer.get(category);
+		}				
 		return answer ;
 	}
 	
@@ -100,7 +144,7 @@ public class ChatbotController {
 	private DoccatModel trainCategorizerModel() throws FileNotFoundException, IOException {
 		// faq-categorizer.txt is a custom training data with categories as per our chat
 		// requirements.
-		InputStreamFactory inputStreamFactory = new MarkableFileInputStreamFactory(new File("faq-categorizer.txt"));
+		InputStreamFactory inputStreamFactory = new MarkableFileInputStreamFactory(new File("faq-categorizer2.txt"));
 		ObjectStream<String> lineStream = new PlainTextByLineStream(inputStreamFactory, StandardCharsets.UTF_8);
 		ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
 
@@ -139,7 +183,6 @@ public class ChatbotController {
 			System.out.println("Tokenizer : " + Arrays.stream(tokens).collect(Collectors.joining(" | ")));
 
 			return tokens;
-
 		}
 	}
 	
